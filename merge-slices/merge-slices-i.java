a26edc31d3bff376b58f8f2da87310aa2938a6c0
Nigel Nquande <nigel.nquande@gmail.com>
2017-10-18 11:30:36 +0200

    Add first Clojure and imperative Java example

---
+++ merge-slices/merge-slices-i.java
@@ -0,0 +1,29 @@
+// Time taken: 25 minutes to write and test, of which 5 were looking at documentation
+
+package clojurevs;
+
+import java.util.Arrays;
+
+public class MergeSlices {
+
+	private static int[] makeMergedSlices() {
+		int[] nums = {1, 2, 3, 4, 5};
+		int[] first = Arrays.copyOfRange(nums, 0, 2);
+		int[] second = Arrays.copyOfRange(nums, 3, nums.length);
+		int[] merged = new int[first.length + second.length];
+		int i = 0, offset = first.length;
+		for (i = 0; i < offset; i++) merged[i] = first[i];
+		for (i = 0; i < second.length; i++) merged[offset + i] = seconds[i];
+		
+		return merged;
+	}
+
+	public static void main(String[] args) { // not C Style, GOK why
+		int[] ms = makeMergedSlices();
+		for (int i = 0; i < ms.length; i++) {
+			System.out.print(String.valueOf(merged[i]));
+			if (i < merged.length -1) System.out.print(' ');
+		}
+	}
+}
+
---
+++ merge-slices/merge-slices.clj
@@ -0,0 +1,8 @@
+;; clojure
+ (vec
+  (concat
+    (subvec [1, 2, 3, 4, 5] 0, 2)
+    (subvec [1, 2, 3, 4, 5] 3)
+  )
+)
+
