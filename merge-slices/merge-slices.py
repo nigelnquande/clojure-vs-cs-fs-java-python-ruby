6fddf46217acd86496b41759d3f72ed937e6661d
Nigel Nquande <nigel.nquande@gmail.com>
2017-10-23 17:55:14 +0200

    Update merge-slices.py
    Use the `filter` function

--- merge-slices/merge-slices.py
+++ merge-slices/merge-slices.py
@@ -1,15 +1,18 @@
-# less than a minute for the Python version
-# 14 minutes of looking at https://docs.python.org/3/howto/functional.html for the iterator version
+# less than a minute for each of the Python versions (imperative/functional)
+# 14 minutes of looking at https://docs.python.org/3/howto/functional.html for the iterator version, 2 minutes of same for filter version
 
 # imperative
 def i ():
   nums = [1, 2, 3, 4, 5]
   print nums[:2] + nums[3:]
   
+def is_allowed (x):
+  return x == 3
+  
 def f ():
-  print([i for i in (1, 2, 3, 4, 5) if i != 3])
+  return list(filter(is_allowed, range(6)))
 
 
 if __name__ == "__main__":
   i()
-  f()
+  print(f())
