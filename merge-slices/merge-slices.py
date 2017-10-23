6b082d81bca558d1ed3d52090fe9d552a9cef089
Nigel Nquande <nigel.nquande@gmail.com>
2017-10-23 17:17:25 +0200

    Update merge-slices.py

--- merge-slices/merge-slices.py
+++ merge-slices/merge-slices.py
@@ -1,3 +1,8 @@
-nums = [1, 2, 3, 4, 5]
-print nums[:2] + nums[3:]
-# less than a minute for the Python version
+# less than a minute for the Python version (imperative)
+# imperative
+def i ():
+  nums = [1, 2, 3, 4, 5]
+  print nums[:2] + nums[3:]
+
+if __name__ == "__main__":
+  i()
