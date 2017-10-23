835323eb9c05b27283668df9f53a3af50f87347c
Nigel Nquande <nigel.nquande@gmail.com>
2017-10-23 17:35:41 +0200

    Update merge-slices.py

--- merge-slices/merge-slices.py
+++ merge-slices/merge-slices.py
@@ -1,8 +1,15 @@
-# less than a minute for the Python version (imperative)
+# less than a minute for the Python version
+# 14 minutes of looking at https://docs.python.org/3/howto/functional.html for the iterator version
+
 # imperative
 def i ():
   nums = [1, 2, 3, 4, 5]
   print nums[:2] + nums[3:]
+  
+def f ():
+  print([i for i in (1, 2, 3, 4, 5) if i != 3])
+
 
 if __name__ == "__main__":
   i()
+  f()
